366f2d6e186bf4aa458f374701ad1b2132826c89
Vincent <stevekeer3310@gmail.com.tw>
2018-10-10 20:22:39 +0800

    spring-demo-aop other feature

---
+++ spring-demo-aop-pointcut-declarations/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,42 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// read spring config java class
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		// get membership bean from spring container
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+		
+		// call the business method
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+		
+		// call the membership business method
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+		
+//		// do it again!
+//		System.out.println("\n let's call it again!");
+//		
+//		// call the business method again
+//		theAccountDAO.addAccount();
+		
+		// close the context
+		context.close();
+	}
+
+}
